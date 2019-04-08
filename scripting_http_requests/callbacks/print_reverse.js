95964078c2a3cb3a88ba556c9d6566774be8ef28
Lzduque <lzduque@hotmail.com>
2019-04-08 17:29:58 +0000

    step 6 of scripting_http_request / reverse

--- scripting_http_requests/callbacks/print_reverse.js
+++ scripting_http_requests/callbacks/print_reverse.js
@@ -1,3 +1,14 @@
 var getHTML = require('../http-functions').getHTML; //('./http-functions') that is exported and now is an object - so you need to call the property you want.
-var printHTML = require('../http-functions').printHTML;
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/reverse.html'
+};
+
+function printReverse (html) {
+  console.log(html.split("").reverse().join(""));
+}
+
+
+getHTML(requestOptions, printReverse);
 
