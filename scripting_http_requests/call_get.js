f86508c4fbf31e442bd866bd2a6a87e545b58f23
Lzduque <lzduque@hotmail.com>
2019-04-08 16:48:44 +0000

    step 5 of scripting_http_request

--- scripting_http_requests/call_get.js
+++ scripting_http_requests/call_get.js
@@ -1,4 +1,4 @@
-var getHTML = require('./http-functions').getHTML;
+var getHTML = require('./http-functions').getHTML; //('./http-functions') that is exported and now is an object - so you need to call the property you want.
 var printHTML = require('./http-functions').printHTML;
 
 var requestOptions = {
